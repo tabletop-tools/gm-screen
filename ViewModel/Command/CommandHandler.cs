dd37be1ccb01b50a2fe79dc6bafb75baa133b531
abstract <chas.williams@gmail.com>
2019-11-15 13:24:13 -0500

    implemented basic scaffolding second pass - viewmodel

---
+++ ViewModel/Command/CommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TabletopTools.GMScreen.ViewMOdel.Command
+{
+    class CommandHandler : ICommand
+    {
+        private readonly Action action;
+
+        public CommandHandler(Action action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+
+    class CommandHandlerParameter<T> : ICommand
+    {
+        private readonly Action<T> action;
+
+        public CommandHandlerParameter(Action<T> action)
+        {
+            this.action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            action((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+}
---
+++ ViewModel/Command/ObservableObject.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TabletopTools.GMScreen.ViewMOdel.Command
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetValue<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (property != null && property.Equals(value))
+                return;
+
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
